444fb0b3cb08b69c96f8d762b70d9af2bc88190e
Angell Belger <angellbelger@hotmail.com>
2021-12-10 14:55:44 -0300

    Update test.py

--- test.py
+++ test.py
@@ -2,16 +2,3 @@ import datetime
 
 # test 05
 
-
-pessoas = dict()
-individuo = dict()
-
-
-while True:
-    individuo["name"] = str(input('nome: '))
-    individuo["age"] = int(input('age: '))
-    pessoas["individuo"] = individuo
-    print(pessoas)
-    #for c in range(0, len(pessoas)):
-     #   print(f'{c} - {pessoas[c]["name"] - pessoas[c]["age"]}')
-
