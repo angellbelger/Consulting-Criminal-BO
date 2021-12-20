921e89211ce266c3f1b6c751bb8561aac443ba74
Angell Belger <angellbelger@hotmail.com>
2021-12-20 14:17:58 -0300

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,9 @@ import datetime
 
 # test 09
 
-x = 3
+lst = [['find it'], 2, 3, 5]
 
-lst = [1, 2, 3, 4, 5]
-
-for c in range(0, len(lst)):
-    if x == lst[c]:
-        print(f'{x} and {lst[c]}')
-    else:
-        print(':(')
+print(lst[0])
 
 
 
